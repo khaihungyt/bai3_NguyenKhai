912269de9ec23cde798cbdef6e148efae845f7c1
Khải Nguyễn <khaihungyt@gmail.com>
2024-11-01 08:49:57 +0700

    fix lan 2

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -14,21 +14,21 @@ export class OrdersController {
         this.lock = new AsyncLock();
     }
     @Post()
-    @UseGuards(JwtGuard, new RoleGuard(Role.CUSTOMER))
+    // @UseGuards(JwtGuard, new RoleGuard(Role.CUSTOMER))
     async CreateOrderABook(
         @Body() createOrderDto: CreateOrderABookInput,
         @Req() req: Request) {
         return await this.lock.acquire("resource-key", async () => {
-            let authorization = req.headers['authorization'];
-            const token = authorization.split(" ")[1];
-            if (token) {
-                try {
-                    const user: any = jwt.verify(token, "Khaideptrai1972");
-                    return await this.orderService.createOrderABook(createOrderDto, "df1f04b4-ef20-489f-af28-c4a805404642");
-                } catch (error) {
-                    throw new HttpException("Invalid Token: " + error.message, HttpStatus.UNAUTHORIZED)
-                }
+            //let authorization = req.headers['authorization'];
+            //const token = authorization.split(" ")[1];
+            //if (token) {
+            try {
+                //const user: any = jwt.verify(token, "Khaideptrai1972");
+                return await this.orderService.createOrderABook(createOrderDto, "df1f04b4-ef20-489f-af28-c4a805404642");
+            } catch (error) {
+                throw new HttpException("Invalid Token: " + error.message, HttpStatus.UNAUTHORIZED)
             }
+            //  }
         });
     }
 }
